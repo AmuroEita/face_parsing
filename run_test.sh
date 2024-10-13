21cb5613af5683d1d273b5a5f9e311da1c1ef80b
Kwan-Yiu Tung <1071307515@qq.com>
2024-10-13 18:39:41 +0800

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=$1 python -u main.py --batch_size 4 --imsize 512 --version parsenet --train False
+CUDA_VISIBLE_DEVICES=$1 python -u main.py --batch_size 1 --imsize 512 --version parsenet --train False
 
 
 
